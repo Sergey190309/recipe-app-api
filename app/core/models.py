04f06ddc1f4705d741aaa782ab0f95687d9eac09
Sergey190309 <sa6702@gmail.com>
2020-01-29 17:25:21 +0300

    Added custom user

--- app/core/models.py
+++ app/core/models.py
@@ -1,16 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User mast provide valid e-vail address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, password):
+        """"Creates and saves new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
